efce51d6a2722e23d9e525c92ebb215772182533
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-06-15 21:44:23 +0200

    Add project files.

---
+++ CarWorkshopMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CarWorkshopMVC.Models;
+
+namespace CarWorkshopMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        var model = new About()
+        {
+            Title = "CarWorkshop app",
+            Description = "Website based on a car workshops.",
+            Tags = new List<string>() { "car", "app", "free" }
+        };
+        return View(model);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
