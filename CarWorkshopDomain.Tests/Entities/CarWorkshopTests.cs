6ec02ce8d5c0434fb87f2753c14a432aae563d72
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-07-07 16:24:24 +0200

    Added: Domain and Application layer XUnit tests

---
+++ CarWorkshopDomain.Tests/Entities/CarWorkshopTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using CarWorkshopDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace CarWorkshopDomain.Entities.Tests
+{
+    public class CarWorkshopTests
+    {
+        [Fact()]
+        public void EncodeName_ShouldSetEncodedName()
+        {
+            // arrange
+            var carWorkshop = new CarWorkshop();
+            carWorkshop.Name = "Test Workshop";
+
+            // act
+            carWorkshop.EncodeName();
+
+            // assert
+            carWorkshop.EncodedName.Should().Be("test-workshop");
+        }
+
+        [Fact()]
+        public void EncodeName_ShouldThrowException_WhenNameIsNull()
+        {
+            // arrange
+            var carWorkshop = new CarWorkshop();
+
+            // act
+            Action action = () => carWorkshop.EncodeName();
+
+            // assert
+            action.Invoking(a => a.Invoke())
+                .Should().Throw<NullReferenceException>();
+        }
+    }
+}
