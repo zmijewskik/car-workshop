a969feb35b93a7c5c2d34caed24c17ffcd251df2
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2023-07-10 20:46:01 +0200

    Added: controller About action test

---
+++ CarWorkshopMVC.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Xunit;
+
+namespace CarWorkshopMVC.Controllers.Tests
+{
+    public class HomeControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> factory;
+
+        public HomeControllerTests(WebApplicationFactory<Program> factory)
+        {
+            this.factory = factory;
+        }
+
+        [Fact()]
+        public async Task About_ReturnsViewWithRenderModel()
+        {
+            // arrange
+            var client = factory.CreateClient();
+
+            // act
+            var response = await client.GetAsync("/Home/About");
+
+            // assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var content = await response.Content.ReadAsStringAsync();
+            content.Should().Contain("<h1>CarWorkshop app</h1>")
+                .And.Contain("<div class=\"alert alert-primary\">Website based on a car workshops.</div>")
+                .And.Contain("<li>car</li>")
+                .And.Contain("<li>app</li>")
+                .And.Contain("<li>free</li>");
+
+        }
+    }
+}
